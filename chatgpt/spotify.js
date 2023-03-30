25a31a6cddbb0ccf435cbecf9fd57835ac369942
nethanielGM <nethaniel@Nethaniels-MacBook-Air.local>
2023-03-30 21:52:27 +0300

    refactor

--- chatgpt/spotify.js
+++ chatgpt/spotify.js
@@ -2,26 +2,56 @@
 
 import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
 import { getUserById } from "./common/getUserById.mjs";
+
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { AWS_DYNAMO } from "./common/config.mjs";
+
 const client = new LambdaClient({ region: 'us-east-1' });
 
 const runApp = async () => {
+
     const user_id = process.env.ADMIN_ID;
     const user = await getUserById(user_id);
     let token = user.spotify_access_token
+    let tokens
     const remainingTime = user.spotify_expiration_timestamp - new Date().getTime();
     const isTokenExpiringSoon = remainingTime <= 30 * 60 * 1000;
+
+
     if (isTokenExpiringSoon) {
-        token = await getToken({
+        console.log('Token is expiring soon, refreshing...');
+        tokens = await getToken({
             FunctionName: 'spotify-token-dev',
             Payload: JSON.stringify({ user_id })
         });
+        tokens = JSON.parse(tokens);
+        try {
+            const documentClient = DynamoDBDocument.from(new DynamoDB(AWS_DYNAMO));
+            user.spotify_access_token = tokens.access_token;
+            console.log('tokens ', tokens);
+            console.log('access_token ', tokens.access_token);
+            if (tokens?.expirationTimestamp) {
+                console.log('expiration_timestamp_spotify ', tokens.expirationTimestamp);
+                user.expiration_timestamp_spotify = tokens.expirationTimestamp;
+            }
+            if (tokens?.refresh_token) {
+                console.log('refresh_token ', tokens.refresh_token);
+                user.refresh_token_spotify = tokens.refresh_token;
+            }
+            await documentClient.put({ TableName: "users", Item: user });
+            token = tokens.access_token
+        } catch (err) {
+            console.error(`Error saveTokens: ${err}`);
+            throw err;
+        }
     }
-    console.log(token);
+
+
     const playlist = await getPlaylist({
         FunctionName: 'spotify-get-playlist-dev',
         Payload: JSON.stringify({ token, playlistName: 'likes' })
     });
-
     console.log(playlist);
 }
 
--- spotify/tracks-api/get-playlist/app.mjs
+++ spotify/tracks-api/get-playlist/app.mjs
@@ -2,33 +2,57 @@ import axios from "axios";
 
 const runApp = async (event) => {
   try {
-    const { token, playlistName } = event
+    const { token, playlistName } = event;
     console.log(token, playlistName);
     const userData = await getUserData(token);
-    console.log(userData);
-    return { statusCode: 200, body: userData }
+
+    // get the current user's playlists
+    const userPlaylists = await getUserPlaylists(token);
+    console.log(userPlaylists);
+
+    return { statusCode: 200, body: userData };
   } catch (err) {
-    console.log('Error getting playlist:');
+    console.log("Error getting playlist:");
     console.log(err);
   }
-}
+};
 
 const getUserData = async (token) => {
   try {
-    const response = await axios.get('https://api.spotify.com/v1/me', {
+    const response = await axios.get("https://api.spotify.com/v1/me", {
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
     });
     console.log(response);
     return response.data;
   } catch (error) {
     console.log(error);
-    return error.data
+    return error.data;
+  }
+};
+
+const getUserPlaylists = async (token, limit = 20, offset = 0) => {
+  try {
+    const response = await axios.get("https://api.spotify.com/v1/me/playlists", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        limit,
+        offset,
+      },
+    });
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return error.data;
   }
 };
 
 export { runApp };
-
