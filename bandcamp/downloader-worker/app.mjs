9824c3017249ed0517593a58eb5e12169c6f9043
nethanielGM <nethaniel@Nethaniels-MacBook-Air.local>
2023-04-02 18:18:40 +0300

    refactor

--- bandcamp/downloader-worker/app.mjs
+++ bandcamp/downloader-worker/app.mjs
@@ -41,6 +41,7 @@ const processAndSaveAlbum = async (album, tableName) => {
 };
 
 const generateAlbumDetails = async (linkInfo, tracksS3) => {
+    console.log(tracksS3);
     const { imageUrl } = await saveImageToS3({ imageUrl: linkInfo.imageUrl, album: linkInfo.name, artist: linkInfo.artist.name });
     let saved = false
     if (tracksS3.length) {
