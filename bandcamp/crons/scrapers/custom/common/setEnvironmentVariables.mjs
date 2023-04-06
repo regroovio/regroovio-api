38413b1e1f3d9d01ce07697bf2be9815dce01112
nethanielGM <nethaniel@Nethaniels-MacBook-Air.local>
2023-04-06 14:43:42 +0300

    refactor

--- bandcamp/crons/scrapers/custom/common/setEnvironmentVariables.mjs
+++ bandcamp/crons/scrapers/custom/common/setEnvironmentVariables.mjs
@@ -3,6 +3,19 @@
 import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
 
 const client = new SSMClient({ region: "us-east-1" });
+
+const parseEnvironmentVariables = (envString) => {
+    const keyValuePairs = envString.split(',');
+    const envObject = {};
+
+    keyValuePairs.forEach(pair => {
+        const [key, value] = pair.split('=');
+        envObject[key.trim()] = value.trim();
+    });
+
+    return envObject;
+}
+
 const setEnvironmentVariables = async () => {
     const params =
     {
@@ -13,7 +26,7 @@ const setEnvironmentVariables = async () => {
     try {
         const data = await client.send(command);
         console.log(data.Parameter.Value);
-        const parsedData = JSON.parse(data.Parameter.Value);
+        const parsedData = parseEnvironmentVariables(data.Parameter.Value);
         process.env = { ...process.env, ...parsedData };
     } catch (error) {
         console.log(error);
