c722ff1ccaeb651684dfe994ee73049c96d75cba
nethanielGM <nethaniel@Nethaniels-MacBook-Air.local>
2023-05-02 10:02:22 +0300

    s3 saving keys

--- main/app/downloader/s3.mjs
+++ main/app/downloader/s3.mjs
@@ -3,56 +3,36 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import axios from 'axios';
 
-const encodeS3Key = (key) => {
-    return key.replace(/ /g, '+');
-}
-
-const saveAlbumToS3 = async (item) => {
-    const { stream, name, album, artist } = item;
+const saveToS3 = async (url, artist, album, name, type) => {
     const s3 = new S3Client({ region: 'us-east-1' });
     const bucketName = `albums-regroovio`;
-    const type = "mp3";
     try {
-        const response = await axios.get(stream, { responseType: 'arraybuffer' });
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
         const buffer = Buffer.from(response.data, 'binary');
         const params = {
             Bucket: bucketName,
-            Key: encodeS3Key(`artists/${artist.replace('/', '-')}/${album.replace('/', '-')}/${name.replace('/', '-')}.${type}`),
+            Key: `artists/${encodeURIComponent(artist)}/${encodeURIComponent(album)}/${encodeURIComponent(name)}.${type}`,
             Body: buffer,
             ContentType: response.headers['content-type']
         };
         await s3.send(new PutObjectCommand(params));
-        const track = `https://${bucketName}.s3.amazonaws.com/${params.Key}`
-        console.log(`Saved track to S3: ${track}`);
-        return { url: track, name };
+        const savedUrl = `https://${bucketName}.s3.amazonaws.com/${params.Key}`
+        console.log(`Saved ${type} to S3: ${savedUrl}`);
+        return savedUrl;
     } catch (err) {
-        console.error(`Error saving album to S3: ${err}`);
+        console.error(`Error saving ${type} to S3: ${err}`);
         return null;
     }
 };
 
+const saveAlbumToS3 = async (item) => {
+    const { stream, name, album, artist } = item;
+    return await saveToS3(stream, artist, album, name, 'mp3');
+};
+
 const saveImageToS3 = async (item) => {
     const { imageUrl, album, artist } = item;
-    const s3 = new S3Client({ region: 'us-east-1' });
-    const bucketName = `albums-regroovio`;
-    const type = "jpg";
-    try {
-        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-        const buffer = Buffer.from(response.data, 'binary');
-        const params = {
-            Bucket: bucketName,
-            Key: encodeS3Key(`artists/${artist.replace('/', '-')}/${album.replace('/', '-')}/${album.replace('/', '-')}.${type}`),
-            Body: buffer,
-            ContentType: response.headers['content-type']
-        };
-        await s3.send(new PutObjectCommand(params));
-        const image = `https://${bucketName}.s3.amazonaws.com/${params.Key}`
-        console.log(`Saved image to S3: ${image}`);
-        return image;
-    } catch (err) {
-        console.error(`Error saving image to S3: ${err}`);
-        return null;
-    }
+    return await saveToS3(imageUrl, artist, album, album, 'jpg');
 };
 
 export { saveAlbumToS3, saveImageToS3 };
--- main/app/worker/downloader.py
+++ main/app/worker/downloader.py
@@ -2,11 +2,13 @@ import os
 import time
 import boto3
 import threading
+import json
+import decimal
+import invoke_lambda
 from dotenv import load_dotenv
 
 import list_tables
 import fetch_unsaved_albums
-import invoke_lambdas_in_chunks
 
 load_dotenv()
 s3 = boto3.client('s3', region_name='us-east-1')
@@ -14,18 +16,38 @@ s3 = boto3.client('s3', region_name='us-east-1')
 CHECK_INTERVAL = 60 * 60  # Check every 1 hour
 
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return float(obj)
+        return super(DecimalEncoder, self).default(obj)
+
+
 def process_albums_for_table_downloader(table_name):
     print(f"Getting {table_name}...")
-    print(f"Retrieving unsaved albums from {table_name}")
-    unsaved_albums = fetch_unsaved_albums.fetch_unsaved_albums(table_name)
-    if not unsaved_albums:
+    chunk_size = 10
+    albums = fetch_unsaved_albums.fetch_unsaved_albums(table_name)
+    if not albums:
         print({"message": "No unsaved albums found."})
-    else:
-        print('')
-        print(f"Found {len(unsaved_albums)} unsaved albums.")
-        invoke_lambdas_in_chunks.invoke_lambdas_in_chunks(
-            f"regroovio-downloader-{os.getenv('STAGE')}", unsaved_albums, table_name
-        )
+        return
+    if len(albums) < chunk_size:
+        chunk_size = len(albums)
+    for i in range(0, len(albums), chunk_size):
+        chunk = albums[i: i + chunk_size]
+        print(
+            f"Downloading chunk {i // chunk_size + 1} of {len(albums) // chunk_size +1}")
+        for album in chunk:
+            invoke_lambda.invoke_lambda(
+                {
+                    "FunctionName": f"regroovio-downloader-{os.getenv('STAGE')}",
+                    "Payload": json.dumps(
+                        {
+                            "tableName": table_name,
+                            "album": album
+                        }, cls=DecimalEncoder
+                    ),
+                }
+            )
 
 
 def downloader_worker(table_name):
