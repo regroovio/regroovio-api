1920e6811f54f373a1cebab233da6a923fbcc86c
nethanielGM <nethaniel@Nethaniels-MacBook-Air.local>
2023-05-05 12:10:18 +0300

    test

--- main/app/test/local.mjs
+++
@@ -1,6 +0,0 @@
-import { handler } from './index.mjs'
-const start = async () => {
-    const response = await handler({ queryStringParameters: { popularity: 30 } })
-    console.log(response)
-}
-start()
