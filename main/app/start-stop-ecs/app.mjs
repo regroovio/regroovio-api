cb8aff90937321fe8bd7109d7bdee002dbee68b3
Nethaniel Gabay <nethanielgabay@Nethaniels-MacBook-Pro.local>
2023-08-13 19:55:39 +0300

    refactor

--- main/app/start-stop-ecs/app.mjs
+++ main/app/start-stop-ecs/app.mjs
@@ -5,12 +5,12 @@ import { exec } from 'child_process';
 const app = async (event) => {
     const { action } = event
     return new Promise((resolve, reject) => {
-        exec(`echo "${process.env.GH_TOKEN}" | gh auth login --with-token`, (authError) => {
+        eexec(`echo "${process.env.GH_TOKEN}" | ./gh auth login --with-token`, (authError) => {
             if (authError) {
                 console.error(`Authentication Error: ${authError}`);
                 return reject(authError);
             }
-            exec(`gh workflow run start-stop.yml --ref main -f stage=${process.env.STAGE} -f action=${action}`, (error, stdout, stderr) => {
+            exec(`./gh workflow run start-stop.yml --ref main -f stage=${process.env.STAGE} -f action=${action}`, (error, stdout, stderr) => {
                 if (error) {
                     console.error(`Error executing gh CLI: ${error}`);
                     return reject(error);
