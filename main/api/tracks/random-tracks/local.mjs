9d2261d00893b9211bf89888529aeeea02493018
Nethaniel Gabay <nethanielgabay@Nethaniels-MacBook-Pro.local>
2023-10-01 09:18:19 +0300

    ci

--- main/api/tracks/random-tracks/local.mjs
+++ main/api/tracks/random-tracks/local.mjs
@@ -1,9 +1,10 @@
 import { handler } from './index.mjs'
 const start = async () => {
     const response = await handler({
-        queryStringParameters: {
-            popularity: 10,
-        }
+        // queryStringParameters: {
+        //     popularity: 10,
+        //     genres: ["techno", "ambient", "punk", "pop", "hip-hop"]
+        // }
     })
     console.log(response);
 }
