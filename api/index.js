16b50404229657d81f23aac315e08bb3cf1c601c
Nethaniel Gabay <nethaniel@whist.co.il>
2023-12-29 20:43:27 +0200

    init

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js'
 import trackRoutes from './routes/tracks.routes.js'
+import usersRoutes from './routes/users.routes.js'
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use('/users', usersRoutes);
 app.use('/tracks', trackRoutes);
 app.get('/health', (req, res) => res.send({ status: 'ok' }));
 
